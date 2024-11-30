cbe7a3a974c538b245504b4d6f0c1d22711e63d3
Walle <Wallinder@users.noreply.github.com>
2024-11-30 15:02:52 +0100

    Update opcodes.py

--- src/opcodes.py
+++ src/opcodes.py
@@ -4,7 +4,7 @@ class opcodes:
     IDENTIFY	                = 2  #  Send	      Starts a new session during the initial handshake.
     PRESENCE_UPDATE	            = 3  #  Send          Update the client's presence.
     VOICE_STATE_UPDATE          = 4  #  Send          Used to join/leave or move between voice channels.
-    RESUME	                    = 6  #  Send          Resume a previous session that was disconnected.
+    RESUME                      = 6  #  Send          Resume a previous session that was disconnected.
     RECONNECT                   = 7  #  Receive       You should attempt to reconnect and resume immediately.
     REQUEST_GUILD_MEMBERS       = 8  #  Send          Request information about offline guild members in a large guild.
     INVALID_SESSION             = 9  #  Receive       The session has been invalidated. You should reconnect and identify/resume accordingly.
