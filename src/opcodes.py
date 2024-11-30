dd3ff8977692dc987570f4fab4b483fbd10e572f
Walle <Wallinder@users.noreply.github.com>
2024-11-30 15:03:02 +0100

    Update opcodes.py

--- src/opcodes.py
+++ src/opcodes.py
@@ -2,7 +2,7 @@ class opcodes:
     DISPATCH	                = 0  #  Receive       An event was dispatched.
     HEARTBEAT	                = 1  #  Send/Receive  Fired periodically by the client to keep the connection alive.
     IDENTIFY	                = 2  #  Send	      Starts a new session during the initial handshake.
-    PRESENCE_UPDATE	            = 3  #  Send          Update the client's presence.
+    PRESENCE_UPDATE             = 3  #  Send          Update the client's presence.
     VOICE_STATE_UPDATE          = 4  #  Send          Used to join/leave or move between voice channels.
     RESUME                      = 6  #  Send          Resume a previous session that was disconnected.
     RECONNECT                   = 7  #  Receive       You should attempt to reconnect and resume immediately.
